397dbfa07da2fd67073348c5bed416f5286f9835
Urielma Jirón Barahona <urielmajb@gmail.com>
2021-03-13 11:33:04 -0600

    Practica pagina web

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let click = document.getElementById('menu-click');
+let enlaces = document.querySelector('.enlaces-header');
+
+click.addEventListener('click', () => {
+    enlaces.classList.toggle('click');
+});
+
+console.log(enlaces); 
